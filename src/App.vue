ae1563bf3daf4632e8bfe69b0cf5d4922e6461fe
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2021-02-13 13:24:39 +0100

    feat: add 'Home' view

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,13 @@
 <template>
   <div id="app">
-    <lead class="lead" />
-    <div class="instructions">
-      <install-instruction
-        os="Arch Linux"
-        type="client"
-        command="yay -S boltchat-client"
-      />
-      <install-instruction
-        os="Docker"
-        type="server"
-        command="docker run -p 3300:3300 --tty boltchat/server:latest"
-      />
-      <span class="more">
-        more can be found
-        <a
-          href="https://github.com/boltchat/bolt/blob/develop/docs/installation.md"
-          target="_blank"
-          >here</a
-        >.
-      </span>
-    </div>
+    <!-- TODO: navbar -->
+    <router-view />
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-import Lead from "./components/Lead.vue";
-import InstallInstruction from "./components/InstallInstruction.vue";
+import { Vue } from "vue-property-decorator";
 
-@Component({
-  components: {
-    Lead,
-    InstallInstruction
-  }
-})
 export default class App extends Vue {}
 </script>
 
@@ -52,21 +25,5 @@ export default class App extends Vue {}
 a {
   color: inherit;
   text-decoration-style: dotted;
-}
-</style>
-
-<style lang="scss" scoped>
-.lead {
-  margin-bottom: 50px;
-}
-
-.instructions {
-  margin: 0 8%;
-
-  .more {
-    display: inline-block;
-    margin-top: 20px;
-    color: gray;
-  }
 }
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+    <lead class="lead" />
+    <div class="instructions">
+      <install-instruction
+        os="Arch Linux"
+        type="client"
+        command="yay -S boltchat-client"
+      />
+      <install-instruction
+        os="Docker"
+        type="server"
+        command="docker run -p 3300:3300 --tty boltchat/server:latest"
+      />
+      <span class="more">
+        more can be found
+        <a
+          href="https://github.com/boltchat/bolt/blob/develop/docs/installation.md"
+          target="_blank"
+          >here</a
+        >.
+      </span>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import Lead from "@/components/Lead.vue";
+import InstallInstruction from "@/components/InstallInstruction.vue";
+
+@Component({
+  components: {
+    Lead,
+    InstallInstruction
+  }
+})
+export default class Home extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+.lead {
+  margin-bottom: 50px;
+}
+
+.instructions {
+  margin: 0 8%;
+
+  .more {
+    display: inline-block;
+    margin-top: 20px;
+    color: gray;
+  }
+}
+</style>
