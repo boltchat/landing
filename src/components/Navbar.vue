5c40437b61e624f69cc32f153e5f896f8d419fd0
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2021-02-14 13:24:55 +0100

    refactor(navbar): replace `<svg>` element with `<img>`

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,22 +3,7 @@
     <img src="../assets/logo.svg" alt="Bolt logo" class="logo" />
     <ul class="nav-links">
       <a href="">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          class="menu"
-        >
-          <line x1="3" y1="12" x2="21" y2="12"></line>
-          <line x1="3" y1="6" x2="21" y2="6"></line>
-          <line x1="3" y1="18" x2="21" y2="18"></line>
-        </svg>
+        <img src="../assets/menu.svg" alt="Menu" class="menu" />
       </a>
       <li class="nav-item">
         <router-link to="/" class="nav-link">Home</router-link>
